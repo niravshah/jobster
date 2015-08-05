925bf67abb960520c020fd290c51778893f93688
niravshah <nirav.shah83@gmail.com>
2015-08-05 17:32:36 +0000

    T

--- public/speck-js/controllers/v2-leaderboard.js
+++ public/speck-js/controllers/v2-leaderboard.js
@@ -3,6 +3,10 @@ angular.module('Speck').controller('LBoardCtrl', ['$scope', '$http', LBoardCtrl]
 function LBoardCtrl($scope, $http) {
   
   $scope.initLBoard = function(){
+    
+    $scope.locations = [{'val':0,'disp':'London'}];
+    $scope.currLoc = 0;
+    
     $http.get('/mock/leaders.json').then(function(res) {
       $scope.leaders = res.data;
     });
