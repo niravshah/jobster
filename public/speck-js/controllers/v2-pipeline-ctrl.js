d74c621a0bcb385bf796cb3605c719eefe2dbce6
niravshah <nirav.shah83@gmail.com>
2015-08-19 17:15:40 +0000

    Pipeline Filter

--- public/speck-js/controllers/v2-pipeline-ctrl.js
+++ public/speck-js/controllers/v2-pipeline-ctrl.js
@@ -2,36 +2,45 @@ angular.module('Speck').controller('PipelineCtrl', ['$scope', '$http', 'filterFi
 
 function PipelineCtrl($scope, $http, filterFilter) {
   $scope.selected = [];
+  $scope.filters = {
+    'career': true,
+    'culture': true
+  }
   $scope.initPipeline = function() {
     console.log('Init Pipeline');
     $scope.megalist = [];
-    
     $http.get('/mock/men_pipeline.json').then(function(res) {
       $scope.megalist = $scope.megalist.concat(res.data);
       $http.get('/mock/women_pipeline.json').then(function(res) {
-        $scope.megalist = $scope.megalist.concat(res.data);        
-        $scope.megalist.sort(function(x, y){ 
-          if (x.first_name < y.first_name) {
-            return -1;
+        $scope.megalist = $scope.megalist.concat(res.data);
+        for(var it in $scope.megalist) {
+          var item = $scope.megalist[it]
+          item.spec = []
+          for(var sp in item.specs) {
+            item.spec.push(item.specs[sp].value)
           }
-          if (x.first_name > y.first_name) {
-            return 1;
-          }
-          return 0;
-        });
-        
+        }
         $scope.sourced = filterFilter($scope.megalist, {
           status: 'sourced'
         });
         $scope.engaged = filterFilter($scope.megalist, {
-          status: 'engaged'
+          status: 'screened'
         });
         $scope.triggered = filterFilter($scope.megalist, {
-          status: 'triggered'
-        });     
+          status: 'interested'
+        });
       });
     });
-    
-    
+  }
+  
+  $scope.filterFunction = function(searchText) {    
+    if(searchText != undefined){
+    return function(item) {      
+      for(var sp in item.spec){
+        if(item.spec[sp].indexOf(searchText) != -1) return true;
+      }
+       return false;
+      
+    }}
   }
 }
