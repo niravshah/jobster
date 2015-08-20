558bbeef47cc535b5fdd6111c54cbf8258f5a950
niravshah <nirav.shah83@gmail.com>
2015-08-20 21:01:34 +0000

    Pipeline Filters

---
+++ public/bower_components/ngBooleanSearch/source/ngBooleanSearch.js
@@ -0,0 +1,226 @@
+(function (window, angular, undefined) {
+    'use strict';
+
+    var module = angular.module('ngBooleanSearch', []);
+    module.provider('ngBooleanSearch', function(){
+
+        var exTree;
+
+        var andExpression = 'and';
+        var orExpression = 'or';
+        var nonePattern = 'none';
+
+        var patterns = ['aspirations:', 'tag:', 'url:', 'title:'];
+
+        this.$get = [function () {
+
+            // Trims defined characters from begining and ending of the string. Defaults to whitespace characters.
+            var trim = function(input, characters){
+
+               if (!angular.isString(input)) return input;
+               
+               if (!characters) characters = '\\s';
+               
+               return String(input).replace(new RegExp('^' + characters + '+|' + characters + '+$', 'g'), '');
+            };
+
+            // Checks if string is blank or not.
+            var isBlank = function(str){
+
+                if (str === null) str = '';
+                return (/^\s*$/).test(str);
+            };
+
+            // Get all property values from object.
+            var values = function (obj) {
+                var vals = [];
+                for( var key in obj ) {
+                    if ( obj.hasOwnProperty(key) ) {
+                        vals.push(obj[key]);
+                    }
+                }
+                return vals;
+            };
+
+            // Find element by condition.
+            var find = function(data, condition){
+
+                var foundItems = [];
+                angular.forEach(data, function(item){
+                    if(condition(item)){
+                        foundItems.push(item);
+                        return;
+                    }
+                });
+
+                return foundItems[0] || undefined;
+            };
+                            
+            // Check that tag collection contains search.
+            var containsTag = function(tags, patternText){
+
+                var tag = find(tags, function(item){
+                    return item.text.indexOf(patternText) !== -1;
+                });
+
+                return angular.isDefined(tag);
+            };
+
+            // Check that title contains search.
+            var containsTitle = function(title, patternText){
+
+                return title.indexOf(trim(patternText)) !== -1;
+            };
+
+            // Check that url contains search.
+            var containsUrl = function(url, patternText){
+
+                return url.indexOf(trim(patternText)) !== -1;
+            };
+          
+           
+          
+
+            // Check that bookmark could be reached by following expression.
+            var evaluateExpression = function(bookmark, node){
+                if(node.pattern === nonePattern && node.literals.length === 1){
+                    var literal = node.literals[0];
+                    var filteredValue = find(values(bookmark), function(propertyValue){
+                        return propertyValue.toString().indexOf(trim(literal.text)) !== -1;
+                    });
+                    return angular.isDefined(filteredValue);
+                }
+
+                var evaluateFunc;
+              
+                if(node.pattern === 'aspirations:'){
+                    evaluateFunc = function(word){ return containsTag(bookmark.aspirations, word); };
+                }              
+                else if(node.pattern === 'tag:'){
+                    evaluateFunc = function(word){ return containsTag(bookmark.tag, word); };
+                }
+                else if(node.pattern === 'title:'){
+                    evaluateFunc = function(word){ return containsTitle(bookmark.title, word); };
+                }
+                else if(node.pattern === 'url:'){
+                    evaluateFunc = function(word){ return containsUrl(bookmark.url, word); };
+                }
+
+                if(node.literals.length === 1){
+                    return evaluateFunc(node.literals[0].text);
+                }
+
+                var result = true;
+                var exp = andExpression;
+                angular.forEach(node.literals, function(literal){
+
+                    var literalResult = evaluateFunc(literal.text);
+                    if(exp === andExpression)
+                    {
+                        result = result && literalResult;
+                    }
+                    else if(exp === orExpression){
+                        result = result || literalResult;
+                    }
+                    exp = literal.expression;
+                });
+
+                return result;
+            };
+
+            // Generate expression tree from search string.
+            var generateExpressionTree = function(searchText){
+
+                if(isBlank(searchText)) return exTree;
+
+                var searchWords = searchText.split(/(aspirations:|tag:|title:|url:)/);
+                if(searchWords.length === 0)
+                    return exTree;
+
+                exTree = [];
+                var node = { pattern: nonePattern, literals:[] };
+                var literal = { text: '', expression: nonePattern};
+                
+                angular.forEach(searchWords, function(word){
+                    if(isBlank(word)) return;
+
+                    if(word === andExpression){
+                        
+                        literal.expression = andExpression;
+                        return;
+                    }
+                    
+                    var pattern = find(patterns, function(it){ return word.indexOf(it) !== -1; });
+                    if(angular.isDefined(pattern)){
+                        // flush node
+                        if(node.literals.length !== 0) exTree.push(node);
+
+                        // create a new node
+                        node = {                    
+                            pattern: pattern,
+                            literals:[]
+                        };
+
+                        return;
+                    }
+
+                    var exps = word.toLowerCase().split(/(and|or)/);
+                    angular.forEach(exps, function(item){
+                        if(isBlank(word)) return;
+
+                        if(item === andExpression){
+                            if(isBlank(literal.text)) return;
+
+                            literal.expression = andExpression;
+                            node.literals.push(literal);
+
+                            literal = null;
+                        }
+                        else if(item === orExpression){
+                            if(isBlank(literal.text)) return;
+
+                            literal.expression = orExpression;
+                            node.literals.push(literal);
+
+                            literal = null;
+                        }
+                        else{
+                            literal = {
+                                expression: nonePattern,
+                                text: trim(item)
+                            };
+                        }
+                    });
+
+                    if(!isBlank(literal.text)) node.literals.push(literal);
+                });
+
+                exTree.push(node);
+
+                return exTree;
+            };
+
+            // Check that bookmark could be reached by following search text.
+            var filterBookmark = function(bookmark, searchText){
+                    
+                if(!searchText) return true;
+
+                if(this.search !== searchText){
+                    this.search = searchText;
+                    this.generateExpressionTree(this.search);
+                }
+                
+                var failureNode = find(exTree, function(node){
+                    return !evaluateExpression(bookmark, node);
+                });
+
+                return !angular.isDefined(failureNode);
+            };
+
+            return {
+                generateExpressionTree: generateExpressionTree,
+                filterBookmark: filterBookmark
+            };
+        }];
+    }); 
+})(window, window.angular);
--- public/speck-js/controllers/v2-pipeline-ctrl.js
+++ public/speck-js/controllers/v2-pipeline-ctrl.js
@@ -1,7 +1,6 @@
-angular.module('Speck').controller('PipelineCtrl', ['$scope', '$http', 'filterFilter', PipelineCtrl]);
+angular.module('Speck').controller('PipelineCtrl', ['$scope', '$http', 'filterFilter', 'ngBooleanSearch',PipelineCtrl]);
 angular.module('Speck').filter('pipelineFilter', function() {
   return function(input, searchText) {
-    //console.log('pipelineFilter',input, searchText);
     if(typeof searchText != 'undefined') {
       var out = []
       var searchTextLower = searchText.toLowerCase();
@@ -10,19 +9,75 @@ angular.module('Speck').filter('pipelineFilter', function() {
           out.push(ip);
         }
       });
-      console.log('pipelineFilter', searchText, input.length, out.length);
+      //console.log('pipelineFilter', searchText, input.length, out.length);
       return out;
     }
     return input;
   }
 });
+angular.module('Speck').filter('aspirationFilter',['ngBooleanSearch', function(ngBooleanSearch) {
+  return function(input, filters) {
+    if(typeof input != 'undefined') {
+      
+      var srchTxt = 'aspirations: '
+      if(filters['anyAll'] == 'any'){
+        if(filters['career'] == true){
+          srchTxt == 'aspirations: ' ? srchTxt = srchTxt.concat('career') : srchTxt = srchTxt.concat(' OR career');
+        }
+        if(filters['compensation'] == true){
+          srchTxt == 'aspirations: ' ? srchTxt = srchTxt.concat('compensation') : srchTxt = srchTxt.concat(' OR compensation');
+        }
+        if(filters['challenge'] == true){
+          srchTxt == 'aspirations: ' ? srchTxt = srchTxt.concat('compensation') : srchTxt = srchTxt.concat(' OR compensation');
+        }
+        if(filters['culture'] == true){
+          srchTxt == 'aspirations: ' ? srchTxt = srchTxt.concat('culture') : srchTxt = srchTxt.concat(' OR culture');
+        }
+      }else{       
+        if(filters['career'] == true){
+          srchTxt == 'aspirations: ' ? srchTxt = srchTxt.concat('career') : srchTxt = srchTxt.concat(' AND career');
+        }
+        if(filters['compensation'] == true){
+          srchTxt == 'aspirations: ' ? srchTxt = srchTxt.concat('comp') : srchTxt = srchTxt.concat(' AND comp');
+        }
+        if(filters['challenge'] == true){
+          srchTxt == 'aspirations: ' ? srchTxt = srchTxt.concat('challenge') : srchTxt = srchTxt.concat(' AND challenge');
+        }
+        if(filters['culture'] == true){
+          srchTxt == 'aspirations: ' ? srchTxt = srchTxt.concat('culture') : srchTxt = srchTxt.concat(' AND culture');
+        }       
+      }
+      
+      var out = []
+      angular.forEach(input,function(ip){
+        if(ngBooleanSearch.filterBookmark(ip,srchTxt) == true){
+            out.push(ip)
+        }
+      });
+      return out;
+      
+      //console.log('aspirationFilter', input, filters, srchTxt);
+      
+    }
+    return input;
+  }
+}]);
 
-function PipelineCtrl($scope, $http, filterFilter) {
+
+
+function PipelineCtrl($scope, $http, filterFilter,ngBooleanSearch) {
   $scope.selected = [];
   $scope.filters = {
     'career': true,
-    'culture': true
+    'culture': true,
+    'compensation': true,
+    'challenge': true,
+    'anyAll':'all'
   }
+  
+  $scope.filterChange = function(){
+    console.log('filterChange: ', $scope.filters);
+  } 
   $scope.initPipeline = function() {
     console.log('Init Pipeline');
     $scope.megalist = [];
@@ -35,7 +90,11 @@ function PipelineCtrl($scope, $http, filterFilter) {
           item.text = JSON.stringify(item).toLowerCase();
           item.spec = [];
           for(var sp in item.specs) {
-            item.spec.push(item.specs[sp].value);
+            item.spec.push(item.specs[sp].text);
+          }
+          item.asp = [];
+          for(var sp in item.aspirations){
+            item.asp.push(item.aspirations[sp].text);
           }
         }
         $scope.sourced = filterFilter($scope.megalist, {
@@ -47,7 +106,23 @@ function PipelineCtrl($scope, $http, filterFilter) {
         $scope.triggered = filterFilter($scope.megalist, {
           status: 'interested'
         });
+        
         //console.log(JSON.stringify($scope.triggered));
+        
+        
+        /*var srchTxt = 'tag: tag1 OR tag2';
+        var bookmark = {
+          title: 'title',
+          url: 'http://127:0:0:1',
+          tag:[{text: 'tag1'}, {text: 'tag2'}, {text: 'tag3'}]
+        };*/
+        
+        var srchTxt = 'aspirations: comp OR culture';
+        var bookmark = {"id":2,"first_name":"Paul","last_name":"Reynolds","email":"preynolds1@dot.gov","sex":"men","imgNum":61,"status":"interested","aspirations":[{"text":"comp"}],"specs":[{"text":"Senior Java Developer"}]}
+        
+        console.log('ngBooleanSearch', ngBooleanSearch.filterBookmark(bookmark,srchTxt));        
+        
+        
       });
     });
   }
